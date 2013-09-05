57d49139d0782e2da1b7e441a5ef5ecfee9a2dde
Olav Brenna Hansen <post@vasetskiheiser.no>
2013-09-05 18:15:08 +0200

    test3

--- MySecoundApp/src/no/example/mysecoundapp/MainActivity.java
+++ MySecoundApp/src/no/example/mysecoundapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+//testing 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
